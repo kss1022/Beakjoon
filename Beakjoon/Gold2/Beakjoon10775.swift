c06d556cd533732fc49b5728c938a41a22357270
hanhyeongyu <kss1022hhh@naver.com>
2024-03-13 15:52:16 +0900

    Beakjoon10775(Union find)

--- Beakjoon/Gold2/Beakjoon10775.swift
+++ Beakjoon/Gold2/Beakjoon10775.swift
@@ -26,11 +26,12 @@ class Beakjoon10775{
         
     }
 
+    //greedy
     func dockingPlanes(_ ports: Int, _ numPlanes: Int, planes: [Int]){
         var gates = Array(repeating: 0, count: ports + 1)
-
+        
         var results = 0
-
+        
         var i = 0
         var keepGoing = true
         
@@ -54,5 +55,49 @@ class Beakjoon10775{
         
         print(results)
     }
+
+    //union find
+    func dockingPlanes2(_ ports: Int, _ numPlanes: Int, planes: [Int]){
+        var result = 0
+        
+        var parents = Array(repeating: 0, count: ports + 1)
+        for i in 1...ports{
+            parents[i] = i
+        }
+        
+        
+        for i in 0..<numPlanes{
+            let plane = planes[i]
+            
+            let parent = findParent(plane)
+            parents[plane] = parent
+            
+            if parent == 0{
+                break
+            }
+            
+            union(parent-1, parent)
+            result += 1
+        }
+        
+        print(result)
+        
+        //a < b
+        func union(_ a: Int, _ b: Int){
+            let aParent = findParent(a)
+            let bParent = findParent(b)
+            parents[bParent] = aParent
+        }
+        
+        func findParent(_ plane: Int) -> Int{
+            if parents[plane] == plane{
+                return plane
+            }
+            
+            return findParent(parents[plane])
+        }
+        
+    }
+
 
 }
